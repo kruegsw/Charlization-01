94e520ee1a57dbdde0448bccce9bd2ac45afa8ef
kruegsw <stuart.krueger9@gmail.com>
2023-09-23 19:44:31 -0400

    specified transports again on server.js and also added a message to chat window upon connection to help troubleshoot from phone

--- server.js
+++ server.js
@@ -46,7 +46,9 @@ app.use('/users', checkAuthenticated, userRouter)
 app.use('/games', checkAuthenticated, gameRouter)
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+//const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, {'transports': ['websocket', 'polling']}); // https://stackoverflow.com/questions/23946683/socket-io-bad-request-with-response-code0-messagetransport-unknown
+
 
 const initializeSocketIO = require("./connections/socketio")
 initializeSocketIO(io)
